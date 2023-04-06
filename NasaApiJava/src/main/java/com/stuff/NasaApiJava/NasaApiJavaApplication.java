5a64f348add472cc272050e2ead8e410103f2db8
myrka1 <myrkag93@gmail.com>
2023-04-05 18:23:46 -0700

    initial commit

---
+++ NasaApiJava/src/main/java/com/stuff/NasaApiJava/NasaApiJavaApplication.java
@@ -0,0 +1,13 @@
+package com.stuff.NasaApiJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasaApiJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NasaApiJavaApplication.class, args);
+	}
+
+}
---
+++ NasaApiJava/src/test/java/com/stuff/NasaApiJava/NasaApiJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stuff.NasaApiJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NasaApiJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
