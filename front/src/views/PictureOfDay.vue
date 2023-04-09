e1c30757552810a0b37cc92e8d49a691e440321a
myrka1 <myrkag93@gmail.com>
2023-04-08 22:30:19 -0700

    changes to frontend connection

--- front/src/views/PictureOfDay.vue
+++ front/src/views/PictureOfDay.vue
@@ -1,32 +1,30 @@
 <template>
-    <div class="container">
-      <h1>This picture of day</h1>
-      <button v-on:click="atClick" type="submit">Submit</button>
-    </div>
-
-    <div>
-      <div v-for="pic in stuff"
-        v-bind:key="pic.Title"
-        class="info"
-      >
-        <img :src="pic.image" class="photo"/>
-        <p>
-          <strong>{{ pic.title }}</strong> <br>
-          <strong>{{ pic.date }}</strong> <br>
-          <strong>{{ pic.explanation }}</strong> <br>
-        </p>
+  <div>
+      <div class="container">
+        <h1>This picture of day</h1>
+        <button v-on:click="atClick" type="submit">Submit</button>
       </div>
-    </div>
+
+      <div>
+        <div v-for="pic in stuff"
+          v-bind:key="pic.Title"
+          class="info"
+        >
+          <img :src="pic.image" class="photo"/>
+          <h2>{{ pic.title }}</h2> 
+          <p>Date: <strong>{{ pic.date }}</strong></p>
+          <p>Explanation:<strong>{{ pic.explanation }}</strong></p>
+        </div>
+      </div>
+  </div>
 </template>
 
 <script>
-
-import service from '../services/BackendService.js';
+import { BackendService } from '../services/BackendService.js';
 
 export default {
   data() {
     return {
-      search: "",
       stuff: [],
     };
   },
@@ -34,9 +32,13 @@ export default {
     atClick() {
       console.log("button clicked");
       try{
-        service.dailyPicture().then((response) => {
+        BackendService.dailyPicture().then((response) => {
           this.stuff = response.data;
-          this.search = "";
+          console.log("stuff");
+          console.log(response);
+          console.log("stuff:", this.stuff);
+          console.log("");
+
         });
       }
       catch (error) {
@@ -45,5 +47,4 @@ export default {
     }
   },
 }
-
 </script>
