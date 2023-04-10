6485dacbf0c11d92b10c1bf4b60742ff126fbb05
myrka1 <myrkag93@gmail.com>
2023-04-09 17:55:23 -0700

    specific date view connected to backend

--- front/src/views/SpecificDate.vue
+++ front/src/views/SpecificDate.vue
@@ -3,19 +3,44 @@
     <h1>Search for a specific date here.</h1>
     <DateForm @date-selection="dateSelected"/>
   </div>
+
+  <div class="display">
+    <div v-for="pic in stuff"
+          v-bind:key="pic.Title"
+          class="info"
+        >
+          <img :src="pic.image" class="photo"/>
+          <h2>{{ pic.title }}</h2> 
+          <strong>Date:</strong> {{ pic.date }} <br>
+          <strong>Explanation:</strong> {{ pic.explanation }}
+        </div>
+  </div>
 </template>
 
 <script>
 import DateForm from '../components/DateForm.vue';
-//import { BackendService } from '@/services/BackendService';
+import { BackendService } from '@/services/BackendService';
 
 export default {
+  data() {
+    return {
+      stuff: [],
+    };
+  },
   components: {
     DateForm
   },
   methods: {
     dateSelected(date) {
       console.log("Selected date HERE!:", date);
+      try {
+        BackendService.specificPicture(date).then((response) => {
+          this.stuff = response.data;
+        });
+      }
+      catch (error) {
+        //console.error(error);
+      }
     }
   }
 };
@@ -23,5 +48,18 @@ export default {
 </script>
 
 <style scoped>
+img {
+  height: 250px;
+  width: 550px;
+}
 
+.display {
+  margin-right: 10px;
+  margin-left: 20px;
+  padding: 20px;
+}
+
+strong {
+  text-decoration: underline;
+}
 </style>
